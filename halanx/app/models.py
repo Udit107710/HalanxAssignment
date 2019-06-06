768f321a31d269bbdfd571fa46687479b35d5092
Udit107710 <500062348@stu.upes.ac.in>
2019-06-06 13:41:44 +0530

    Models made

---
+++ halanx/app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator, MinLengthValidator, MaxValueValidator, MinValueValidator, MaxValueValidator
+
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Address(models.Model):
+    street_address = models.TextField()
+    city = models.CharField(max_length= 30)
+    state = models.CharField(max_length= 30)
+    pincode = models.IntegerField()
+    country = models.CharField(max_length= 30)
+
+class Profile(models.Model):
+    GENDER_CHOICES = [ ('M', 'Male'), ('F', 'Female'), ('O', 'Other')]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='custom_user_profile')
+    phone_no = models.BigIntegerField(unique=True, validators=[MaxValueValidator(9999999999),MinValueValidator(0000000000)])
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    profile_pic = models.ImageField(upload_to= user_directory_path)
+    date_of_birth = models.DateField()
+    permanent_address = models.OneToOneField(Address, on_delete= models.CASCADE, related_name='permanent_address_is')
+    street_address = models.OneToOneField(Address, on_delete= models.CASCADE, related_name= 'street_address_is')
+    company_address = models.OneToOneField(Address, on_delete= models.CASCADE, related_name= 'company_address_is')
+    friends = models.ManyToManyField(User, related_name= 'friends_are')
